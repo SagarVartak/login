f86083938146ca339cc1d2ff4aef337ee96a7c65
SagarVartak <sagarvartak25@gmail.com>
2021-09-19 01:04:53 +0530

    addeded code for images 14

--- src/components/GetImages.jsx
+++ src/components/GetImages.jsx
@@ -13,38 +13,11 @@ export default function GetImages() {
    },[]);
 //creating state
 
-const [items,setItems]=useState([]);
+const [image,setImage]=useState([]);
 
 const fetchItems =async()=>{
 
-    const data =await fetch("https://raw.githubusercontent.com/SagarVartak/jsondata/main/images.json");
-
-    // console.log(data);
-
-    const items=await data.json();
-    // console.log(items);
-
-    setItems(items);
-
-//    const images = await fetch("https://api.imagekit.io/v1/files", {
-//        method: 'GET',
-//         headers: {
-//              "Authorization": 'Basic' + encode("private_FEQHGrJPnn7Nl/Ymv+UByhUB4jE=:Sagar@992370"),
-//              'Content-Type': 'application/json',
-//              'Access-Control-Allow-Origin': '*',
-                // "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-                 
-//         },
-//     }).then(response => {
-//         console.log("response",response);
-//         return response.json();
-//     }).catch(err => {
-//         console.log(err);
-//         return err;
-//     })
-//     console.log(images);
-
-    const images = await axios.get("https://api.imagekit.io/v1/files",{
+    const newdata = await axios.get("https://api.imagekit.io/v1/files",{
         auth:{
             username:"private_FEQHGrJPnn7Nl/Ymv+UByhUB4jE=",
             password:"Sagar@992370"
@@ -53,16 +26,21 @@ const fetchItems =async()=>{
                     'Content-Type': 'application/json',
                  }
     }).then(res => {
-        console.log(res);
+        console.log("respone",res);
+        return res.data;
     }).catch(err => {
         console.log(err);
     })
 
-    console.log(images);
+    console.log("images",newdata);
+    const image = await newdata;
+    setImage(image);
+
 }
 
     return (
-        <div className="image-row">
+        <div>
+        {/* <div className="image-row">
             <div className="image-column">
                 {items.map(item =>(
                     <a href={item.url}>
@@ -70,6 +48,17 @@ const fetchItems =async()=>{
                     </a>
                 ))}
             </div>
+        </div> */}
+
+        <div className="image-row">
+            <div className="image-column">
+                {image.map(image =>(
+                    <a href={image.url} key={image.fileId}>
+                    <img src={image.url} alt={image.name} loading='lazy'></img>
+                    </a>
+                ))}
+            </div>
+        </div>
         </div>
     )
 }
